a3140245b519caf0819454af00e443ecfb64a492
Zuzanna Popiel <zuzanna.popiel@gmail.com>
2020-08-08 20:04:09 +0200

    Calculator

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+public class Calculator {
+    private Object someMethod;
+
+    public void someMethod() {
+    }
+
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
